3cc1e2ac1e962bda5df17d4dfb40b71cfc61e302
Ilya Maximov <xc.navi@gmail.com>
2023-03-13 16:01:01 +0200

    FlyWay working

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main
+{
+    public static void main ( String [ ] args )
+    {
+//         mvn flyway:baseline -Dflyway.baselineVersion=1 -Dflyway.baselineDescription="Base version"
+//
+//        для начала очистим всё;
+//        используем flyway:clean;
+//        а потом flyway:migrate.
+//        select * from second_test_table;
+    }
+
+}
